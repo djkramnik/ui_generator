b43af05964bef3acb99a9b9dd622be7360f5cebf
David Gurr <davidk.gurr@gmail.com>
2024-08-19 14:19:48 -0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  async rewrites() {
+    return [
+
+    ]
+  },
+}

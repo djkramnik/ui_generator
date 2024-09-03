8f9b88c62bdf4a5fbc966d106b12dcfa657e7ee1
David Gurr <davidk.gurr@gmail.com>
2024-09-03 18:34:18 -0400

    guilt coding

--- components/dynamic/index.tsx
+++ components/dynamic/index.tsx
@@ -1,5 +1,9 @@
 import React, { createElement } from "react"
 import { CSSProperties } from "react"
+import { Box, BoxProps } from "../lib"
+import { CssProps } from "../theme"
+import { useTheme } from "styled-components"
+import { Anchor, Button } from "../lib/atomics"
 
 type TextNode = { 
   tagName: 'text',
@@ -12,23 +16,63 @@ type DomNode = {
   html?: string
   style: CSSProperties
   styleStr: string
+  href?: string
+  src?: string
 } 
 
 type StyleNode = DomNode | TextNode
+
+const toSx = (cssProperties: CSSProperties): CssProps => {
+  return Object.entries(cssProperties)
+    .reduce((acc, [k, v]) => {
+      return {
+        ...acc,
+        k: String(v)
+      }
+    }, {} as CssProps)
+}
+
+// not any.... a component that accepts $sx and theme, or null
+const getElement = (tagName: string): any => {
+  switch(tagName) {
+    case 'a':
+      return Anchor
+    case 'button':
+    // todo... copying certain attributes...
+    case 'input':
+      // todo: styled input...
+      return Button
+    // todo... styled image!
+    case 'img':
+    case 'image':
+      return Button
+    case 'svg':
+      return 'svg'
+    case 'script':
+    case 'link':
+      return null
+    default:
+      return Box
+  }
+}
 
 export const DynamicComponent = ({
   graph
 }: {
   graph: StyleNode
 }) => {
+  const theme = useTheme()
   const { tagName } = graph
   if (tagName === 'text') {
     return (graph as TextNode).content
   }
-
+  const element = getElement(tagName)
+  if (element === null) {
+    return null
+  }
   return createElement(
-    tagName,
-    {$sx: (graph as DomNode).style},
+    element,
+    {$sx: toSx((graph as DomNode).style), theme},
     ...(
       (graph as DomNode).children
         .map((c, index) => {

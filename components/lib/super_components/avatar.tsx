74dc648b2c0177aceb2dc6926e34feaeb74198c5
David Gurr <davidk.gurr@gmail.com>
2024-10-04 13:47:01 -0400

    random avatar now

--- components/lib/super_components/avatar.tsx
+++ components/lib/super_components/avatar.tsx
@@ -1,7 +1,9 @@
-import { useTheme } from "styled-components"
-import { getSuperComponentStyles } from "../../theme"
-import { Box, Flex } from "../layout"
+import { useTheme } from 'styled-components'
+import { getSuperComponentStyles } from '../../theme'
+import { Box, Flex } from '../layout'
 import { Copy, Heading, Image } from '../atomics'
+import { useEffect, useState } from 'react'
+import { randomItem } from '../../utils'
 
 export type AvatarProps = {
   asset: string
@@ -9,69 +11,98 @@ export type AvatarProps = {
   position?: string
 }
 
-export const MyAvatar = ({
-  asset,
-  name,
-  position,
-}: AvatarProps) => {
+const avatars = [
+  'a.jpg',
+  'adam.jpg',
+  'beetle.jpg',
+  'ben.jpg',
+  'bike.jpg',
+  'debbie.jpg',
+  'guillermo.jpg',
+  'guts.jpg',
+  'historian.jpg',
+  'hulk.jpg',
+  'kent.jpg',
+  'monkey.jpg',
+  'monkey2.jpg',
+  'monkey3.jpg',
+  'pilot.jpg',
+  'rp.jpg',
+  'ryan.jpg',
+  'shruti.jpg',
+  'superman.jpg',
+]
+
+export const MyAvatar = ({ asset, name, position }: AvatarProps) => {
   const theme = useTheme()
   return (
-    <Box $sx={{
-      ...getSuperComponentStyles(
-        'avatar',
-        theme,
-      )
-    }}>
-      <Flex $sx={{
-        ...getSuperComponentStyles(
-          'avatarInner',
-          theme,
-        )
-      }}>
-        <Box $sx={{
-          ...getSuperComponentStyles(
-            'avatarLeft',
-            theme,
-          )
-        }}>
-          <Image 
+    <Box
+      $sx={{
+        ...getSuperComponentStyles('avatar', theme),
+      }}
+    >
+      <Flex
+        $sx={{
+          ...getSuperComponentStyles('avatarInner', theme),
+        }}
+      >
+        <Box
+          $sx={{
+            ...getSuperComponentStyles('avatarLeft', theme),
+          }}
+        >
+          <Image
             src={`/avatars/${asset}`}
             $sx={{
-              ...getSuperComponentStyles(
-                'avatarImg',
-                theme,
-              )
+              ...getSuperComponentStyles('avatarImg', theme),
             }}
           />
         </Box>
-        <Flex $sx={{
-          ...getSuperComponentStyles(
-            'avatarRight',
-            theme
-          )
-        }}>
-          <Heading level={4} $sx={{
-            ...getSuperComponentStyles(
-              'avatarBig',
-              theme
-            )
-          }}>
+        <Flex
+          $sx={{
+            ...getSuperComponentStyles('avatarRight', theme),
+          }}
+        >
+          <Heading
+            level={4}
+            $sx={{
+              ...getSuperComponentStyles('avatarBig', theme),
+            }}
+          >
             {name}
           </Heading>
-          {
-            position
-              ? (
-                <Copy $sx={{
-                  ...getSuperComponentStyles(
-                    'avatarLittle',
-                    theme
-                  )
-                }}>{position}</Copy>
-              )
-              : null
-          }
+          {position ? (
+            <Copy
+              $sx={{
+                ...getSuperComponentStyles('avatarLittle', theme),
+              }}
+            >
+              {position}
+            </Copy>
+          ) : null}
         </Flex>
       </Flex>
     </Box>
   )
+}
+
+export const RandomAvatar = ({
+  name,
+  position,
+}: {
+  name: string
+  position?: string
+}) => {
+  const [asset, setAsset] = useState<string | null>(null)
+  useEffect(() => {
+    if (asset !== null) {
+      return
+    }
+    setAsset(randomItem(avatars))
+  }, [setAsset, asset])
+
+  if (!asset) {
+    return
+  }
+  return <MyAvatar asset={asset} name={name} position={position} />
 }
--- components/lib/super_components/search-result.tsx
+++ components/lib/super_components/search-result.tsx
@@ -1,3 +1,8 @@
-export const VideoSearchResult = () => {
+export const VideoSearchResult = ({
+  title,
+  
+}: {
+  title: string
+}) => {
   
 }
